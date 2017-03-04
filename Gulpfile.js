b30cd183a720037b923a6631291314abb2655495
DecaHub <dan@decahub.com>
2017-03-03 20:08:58 -0500

    Create dist folder. Add ability to delete/add .scss at any level within the app folder
    the scss-watch task can now negotiate the matching .css of any .scss at any level within the app folder. 
    Need to create the ability to delete the containing folder if it is empty and also to delete all the .scss files within a folder if a folder is deleted.

--- Gulpfile.js
+++ Gulpfile.js
@@ -54,6 +54,27 @@ const printTask = function (taskName) {
 	
 };
 
+let srcFiles = {
+	
+	scss: [
+		"!app/lib/**",
+		"!app/lib",
+		"app/**/*.scss",
+	],
+	html: [
+		"!app/lib",
+		"!app/lib/**",
+		"app/**/*.html"
+	]
+	
+};
+
+let destDir = {
+	
+	scss: "app/dist/css"
+	
+};
+
 /**
  * Compiles .scss to .css
  */
@@ -61,9 +82,9 @@ gulp.task('sass', function() {
 	
 	printTask("sass");
 	
-	return gulp.src("app/scss/**/*.scss")
+	return gulp.src(srcFiles.scss)
 		.pipe(sass())
-		.pipe(gulp.dest("app/css"));
+		.pipe(gulp.dest(destDir.scss));
 	
 });
 
@@ -71,27 +92,74 @@ gulp.task('sass', function() {
  * Watch scss files for changes.
  * Perform actions based on the file event: added, deleted, changed.
  */
+
 gulp.task('scss-watch', function(){
 
 	printTask("scss-watch");
 
-	let watcher = gulp.watch('app/scss/**/*.scss', function(){
+	let watcher = gulp.watch(srcFiles.scss, function(){
 		console.log("Runninig deleteWatch sequence");
 
 	});
 	
 	watcher.on('change', function(event){
 
-		console.log(`Event type: ${event.type}`)
+		console.log(`Event type: ${event.type}`);
 
 		if (event.type === 'deleted'){
 			console.log(event.path + " is deleted. Deleting corresponding .css files from app/css");
 			
+			console.log(event.path);
+			
+			let fullPath = event.path;
+			let rootToCSS = "app/dist/css/";
 			let fileNameBase = path.basename(event.path, '.scss');
+			let pathToCSS = "";
+			let fullPathToCSS = "";
 			
-			console.log(fileNameBase);
+			let fullPathArray = fullPath.split("/");
+			let index = 0;
 			
-			del(['app/css/' + fileNameBase + '.*'])
+			console.log(fullPathArray);
+			
+			for (let i = 0; i < fullPathArray.length; i++) {
+				
+				if (fullPathArray[i] === "app") {
+					
+					index = i;
+					
+					console.log(`index: ${index}, ${fullPathArray[i]}`);
+					
+					break;
+					
+				}
+			}
+			
+			for (let i = index; i < fullPathArray.length - 1; i++) {
+				
+				console.log(i);
+
+				if (i > index && i < fullPathArray.length - 1) {
+
+					pathToCSS += fullPathArray[i] + "/";
+
+				}
+
+			}
+			
+			console.log(`Path to CSS: ${pathToCSS}`);
+			console.log(`More complete path: ${rootToCSS}${pathToCSS}${fileNameBase}.css`);
+			
+			fullPathToCSS = rootToCSS + pathToCSS + fileNameBase + ".*";
+			
+			console.log(fullPathToCSS);
+			
+			// console.log(fileNameBase);
+			//
+			// console.log(path.relative('app/dist/css/', event.path));
+			
+			// del([destDir.scss + fileNameBase + '.*'])
+			del(fullPathToCSS)
 				.then(function(paths){
 					console.log("deleted files: " + paths.join('\n'));
 					runSequence('inject');
@@ -116,6 +184,18 @@ gulp.task('scss-watch', function(){
 	})
 });
 
+gulp.task('html-watch', function () {
+	
+	let watcher = gulp.watch(srcFiles.html);
+	
+	watcher.on('change', function (event) {
+		
+		reload();
+		
+	})
+	
+});
+
 /**
  * Injects .scss files into index.html 
  */
@@ -128,7 +208,7 @@ gulp.task('inject', function () {
 	};
 
 	let injectSrc = gulp.src([
-		"app/css/**/*.css"
+		"app/dist/css/**/*.css"
 		], {read: false}
 	);
 
@@ -156,7 +236,7 @@ gulp.task("serve", function () {
  */
 gulp.task("init", function() {
 
-	runSequence('sass', 'inject', 'scss-watch', "serve");
+	runSequence('clean:css','sass', 'inject', 'scss-watch', 'html-watch', "serve");
 
 });
 
@@ -168,3 +248,36 @@ gulp.task("default", function() {
 	runSequence('init');
 
 });
+
+gulp.task("clean:css", function () {
+	
+	return del([
+		"app/dist/css"
+	]);
+	
+});
+
+// let scssReplace2 = function (infoPieces) {
+//
+// 	infoPieces.forEach(function (piece, index) {
+//
+// 		if (piece == "scss") {
+//
+// 			console.log("YES!");
+// 			infoPieces[index] = "css";
+//
+// 		}
+//
+// 	});
+//
+// 	for (let i = 0; i < infoPieces.length; i++) {
+//
+// 		if (infoPieces[i] === "scss") {
+// 			infoPieces[i] = "css"
+// 		}
+//
+// 	}
+//
+// 	return pieces;
+//
+// };
