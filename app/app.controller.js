eb1fcd99cf0830c0a07d3055534f20824dcdc4de
DecaHub <dan@decahub.com>
2017-03-10 19:45:29 -0500

    Create angular main app with controller.

--- app/app.controller.js
+++ app/app.controller.js
@@ -1,3 +1,12 @@
 /**
  * Created by DecaHub on 3/4/17.
  */
+
+"use strict";
+
+angular.module("main").
+	controller("MainController", ["$scope", function ($scope) {
+		
+		
+		
+}]);
--- app/main.app.js
+++ app/main.app.js
@@ -1,8 +1,3 @@
 "use strict";
 
-
-(function () {
-	
-	console.log("LOL");
-	
-}());
+angular.module("main", []);
